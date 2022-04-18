7b1c56d4efdfcc0e72a00fce4d2dd0418af3848d
Alfred Githinji <alfredgithinji87@gmail.com>
2022-04-18 18:10:02 +0300

    Shaping things up a little bit

--- src/components/SwipeButtons.js
+++ src/components/SwipeButtons.js
@@ -9,27 +9,29 @@ import FlashOnIcon from "@material-ui/icons/FlashOn";
 
 function SwipeButtons() {
   return (
-    <div className="swipeButtons">
-      <IconButton className="swipeButtons__repeat">
-        <ReplayIcon />
-      </IconButton>
-      <IconButton
-        onClick={() => {
-          window.location = "https://google.com";
-        }}
-        className="swipeButtons__left"
-      >
-        <KeyboardArrowLeft />
-      </IconButton>
-      <IconButton className="swipeButtons__star">
-        <StarRateIcon />
-      </IconButton>
-      <IconButton className="swipeButtons__right">
-        <KeyboardArrowRightIcon />
-      </IconButton>
-      <IconButton className="swipeButtons__lightning">
-        <FlashOnIcon />
-      </IconButton>
+    <div className="grandpa">
+      <div className="swipeButtons">
+        <IconButton className="swipeButtons__repeat">
+          <ReplayIcon />
+        </IconButton>
+        <IconButton
+          onClick={() => {
+            window.location = "https://google.com";
+          }}
+          className="swipeButtons__left"
+        >
+          <KeyboardArrowLeft />
+        </IconButton>
+        <IconButton className="swipeButtons__star">
+          <StarRateIcon />
+        </IconButton>
+        <IconButton className="swipeButtons__right">
+          <KeyboardArrowRightIcon />
+        </IconButton>
+        <IconButton className="swipeButtons__lightning">
+          <FlashOnIcon />
+        </IconButton>
+      </div>
     </div>
   );
 }
--- src/components/TinderCards.js
+++ src/components/TinderCards.js
@@ -1,68 +1,57 @@
-import React,{useState,useEffect} from 'react';
-import './TinderCards.css';
-import TinderCard from 'react-tinder-card';
-import axios from '../axios';//Makes http request super simple to work with.
-
+import React, { useState, useEffect } from "react";
+import "./TinderCards.css";
+import TinderCard from "react-tinder-card";
+import axios from "../axios"; //Makes http request super simple to work with.
 
 function TinderCards() {
+  //This state or this object array keeps track of all the people.
+  const [people, setPeople] = useState([]);
 
-//This state or this object array keeps track of all the people.
-    const [people,setPeople]= useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const req = await axios.get("tinder/cards");
 
-useEffect(()=>{
-    const fetchData= async () =>{
- const req= await axios.get('tinder/cards');
+      console.log(req);
 
- console.log(req);
+      setPeople(req.data);
+    };
+    fetchData();
+  }, []);
 
- setPeople(req.data);
-    }
-fetchData()
-},[])
+  const swiped = (direction, nameToDelete) => {
+    console.log(`removing ${nameToDelete} , ${direction}`);
+  };
 
-
-
-    const swiped = (direction,nameToDelete)=> {
-
-        console.log(`removing ${nameToDelete} , ${direction}`)
-
-    }
- 
-    const outOfFrame = (name)=>{
-        console.log(` ${name} left the screen! `)
-
-    }
-    return (
-        <div className="tinderCards">
-
-            {/* We kinda loop over the object array and render a tinder card for every person that is in like our box>So lets go... */}
-        <div className="tinderCards_cardContainer">
-          
-            {people.map((person)=>{
-                return (
-                      
-                <TinderCard 
-                className ="swipe"
-                key = {person.name} 
-                preventSwipe= {["up","down"]} 
-                onSwipe = {(dir)=>swiped(dir,person.name)} 
-                onCardLeftScreen= {()=>{outOfFrame(person.name)}}
-                >
-               
-                <div style ={{backgroundImage:`url(${person.imgUrl})`}} 
-                className = "card">
-
-                    <h3>{person.name}</h3>
-                </div>
-                </TinderCard>
-
-            )})}
-             
-            </div>
-
-
-        </div>
-    );
+  const outOfFrame = (name) => {
+    console.log(` ${name} left the screen! `);
+  };
+  return (
+    <div className="tinderCards">
+      {/* We kinda loop over the object array and render a tinder card for every person that is in like our box>So lets go... */}
+      <div className="tinderCards_cardContainer">
+        {people.map((person) => {
+          return (
+            <TinderCard
+              className="swipe"
+              key={person.name}
+              preventSwipe={["up", "down"]}
+              onSwipe={(dir) => swiped(dir, person.name)}
+              onCardLeftScreen={() => {
+                outOfFrame(person.name);
+              }}
+            >
+              <div
+                style={{ backgroundImage: `url(${person.imgUrl})` }}
+                className="card"
+              >
+                <h3>{person.name}</h3>
+              </div>
+            </TinderCard>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default TinderCards;
